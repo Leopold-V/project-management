441c62d384f60c128ca73ad9f76cd4223c513bb8
Leopold-V <leopoldV79@gmail.com>
2021-03-17 23:54:09 +0100

    test firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import app, { auth } from './firebase';
+
 function App() {
+
+  const handleLogin = () => {
+    console.log(auth.signInWithEmailAndPassword('test@gmail.com', 'testtest'));
+  }
+
   return (
     <div className="App">
-      Hello
+      <button onClick={handleLogin}>Test</button>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const app = firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.project-management-22e40.appspot.com,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: "https://"+process.env.REACT_APP_FIREBASE_PROJECT_ID+".firebaseio.com",
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
-
 
 export const auth = app.auth();
 export default app;
