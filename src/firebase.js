69dadddd2d6069f9120ec1499686ac71b0e4f3da
Leopold-V <leopoldV79@gmail.com>
2021-03-17 23:34:00 +0100

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const app = firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.project-management-22e40.appspot.com,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+});
+
+
+export const auth = app.auth();
+export default app;
