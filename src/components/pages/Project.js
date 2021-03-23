639aa1fd07b99ed357b71eb91a4b28df3c80ea45
Leopold-V <leopoldV79@gmail.com>
2021-03-23 12:11:50 +0100

    add resume

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -16,6 +16,7 @@ export const Project = (props) => {
   return (
     <ContainerSection>
       <h2>Project page {projects[0] && projects[0].name}</h2>
+      <p>{projects[0] && projects[0].resume}</p>
       <ContainerCard>
         <CardTask title="Todo" tasks={tasksTodo} />
         <CardTask title="In progress" tasks={tasksInProgress} />
