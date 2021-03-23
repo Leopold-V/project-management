262a1f840690e7b13b652a28cf1588b4ef10ac0b
Leopold-V <leopoldV79@gmail.com>
2021-03-23 12:12:47 +0100

    refactor form components

---
+++ src/components/Form/index.js
@@ -0,0 +1,6 @@
+export { Input } from './Input';
+export { InputGroup } from './InputGroup';
+export { TextArea } from './TextArea';
+export { Icon } from './Icon';
+export { Form } from './Form';
+export { FormAddProject } from './FormAddProject';
