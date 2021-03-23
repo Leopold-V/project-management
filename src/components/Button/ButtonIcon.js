1feb8ade60609d3170b42716e194235c10c7dbed
Leopold-V <leopoldV79@gmail.com>
2021-03-23 12:11:31 +0100

    add button

---
+++ src/components/Button/ButtonIcon.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonIcon = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  padding: 0.7rem 0.8rem;
+  color: white;
+  cursor: pointer;
+  transition: all 0.2s;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
