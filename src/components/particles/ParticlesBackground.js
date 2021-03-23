7949d95baaa6ce79a2a7c38d89174419a9789b4e
Leopold-V <leopoldV79@gmail.com>
2021-03-23 16:28:59 +0100

    add particles component

---
+++ src/components/particles/ParticlesBackground.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components';
+import Particles from 'react-particles-js';
+
+export const ParticlesBackground = () => {
+    return (
+        <ParticlesStyled params={particleParams} />
+    )
+}
+
+export const ParticlesStyled = styled(Particles)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+  @media (max-width: 1000px) {
+    display: none;
+  }
+`;
+
+const particleParams = {
+    particles: {
+      number: {
+        value: 160,
+        density: {
+          enable: false,
+        },
+      },
+      size: {
+        value: 4,
+        random: true,
+        anim: {
+          speed: 4,
+          size_min: 0.3,
+        },
+      },
+      line_linked: {
+        enable: false,
+      },
+      move: {
+        random: true,
+        speed: 1,
+        direction: 'top',
+        out_mode: 'out',
+      },
+    },
+  };
+  
